a2f560c76113ee56fcab1cd5e438bc850ab0d625
davidchung29 <60008214+davidchung29@users.noreply.github.com>
2025-02-07 18:52:44 -0500

    new my eyes that uses uikit

---
+++ myEyes/myEyes/myEyes2.swift
@@ -0,0 +1,8 @@
+//
+//  myEyes2.swift
+//  myEyes
+//
+//  Created by David Jr on 2/7/25.
+//
+
+import Foundation
---
+++ myEyes2-uikit/myEyes2-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myEyes2-uikit
+//
+//  Created by David Jr on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
