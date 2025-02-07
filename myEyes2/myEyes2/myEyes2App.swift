c966c7cdea9d07206bbe2183a2fecbe1db69f4ec
davidchung29 <60008214+davidchung29@users.noreply.github.com>
2025-02-07 18:38:35 -0500

    update myeyes

---
+++ myEyes2/myEyes2/myEyes2App.swift
@@ -0,0 +1,17 @@
+//
+//  myEyes2App.swift
+//  myEyes2
+//
+//  Created by David Jr on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct myEyes2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
