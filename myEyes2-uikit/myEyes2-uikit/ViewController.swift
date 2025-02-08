599dbda2bffa40f8598fbf7447ab90d86f2d48ef
RichieTran <richietran1036@gmail.com>
2025-02-07 20:41:54 -0500

    WE GOT VISION

--- myEyes2-uikit/myEyes2-uikit/ViewController.swift
+++ myEyes2-uikit/myEyes2-uikit/ViewController.swift
@@ -9,12 +9,74 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    var session: AVCaptureSession?
+    let output = AVCapturePhotoOutput()
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    override func viewDidLoad(){
         super.viewDidLoad()
+        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
         // Do any additional setup after loading the view.
+        checkCamPerm()
     }
-
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = view.bounds
+    }
+    private func checkCamPerm()
+    {
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video){ [weak self] granted in
+                guard granted else{
+                    return
+                }
+                DispatchQueue.main.async{
+                    self?.setCamera()
+                }
+            }
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            setCamera()
+        @unknown default:
+            break
+        }
+    }
+    private func setCamera(){
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video){
+            do{
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input){
+                    session.addInput(input)
+                }
+                if session.canAddOutput(output){
+                    session.addOutput(output)
+                }
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                session.startRunning()
+                self.session = session
+            }
+            catch{
+                print(error)
+            }
+        }
+    }
 }
 
+extension ViewController: AVCapturePhotoCaptureDelegate{
+    func photoOutput(output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?){
+        guard let data = photo.fileDataRepresentation() else{
+            return
+        }
+        let image = UIImage(data: data)
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = view.bounds
+        view.addSubview(imageView)
+    }
+}
